547125aba83f660ba380f2711cfcb127c9ce8032
dg.dorjigyeltshen <dg.dorjigyeltshen@gmail.com>
2023-08-13 22:18:59 +0600

    chore(ui): make profile menu not scrollable

--- lib/pages/profile_menu_page.dart
+++ lib/pages/profile_menu_page.dart
@@ -10,34 +10,32 @@ class ProfileMenuPage extends StatelessWidget{
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: SingleChildScrollView(
-          child: SpacedColumn(
-            spaceHeight: 10,
-            children: [
-              const ProfileCard(),
-              const SizedBox(height: 10),
-              ProfileMenuItem(
-                title: 'View Profile',
-                icon: const Icon(Icons.visibility_outlined, color: Color(0xFF626262)),
-                onTap: (){},
-              ),
-              ProfileMenuItem(
-                title: 'Eat Spinner',
-                icon: const Icon(Icons.gamepad_outlined, color: Color(0xFF626262)),
-                onTap: (){},
-              ),
-              ProfileMenuItem(
-                title: 'Share Profile Link',
-                icon: const Icon(Icons.link_outlined, color: Color(0xFF626262)),
-                onTap: (){},
-              ),
-              ProfileMenuItem(
-                title: 'Logout',
-                icon: const Icon(Icons.logout_outlined, color: Color(0xFF626262)),
-                onTap: (){},
-              )
-            ],
-          ),
+        child: SpacedColumn(
+          spaceHeight: 10,
+          children: [
+            const ProfileCard(),
+            const SizedBox(height: 10),
+            ProfileMenuItem(
+              title: 'View Profile',
+              icon: const Icon(Icons.visibility_outlined, color: Color(0xFF626262)),
+              onTap: (){},
+            ),
+            ProfileMenuItem(
+              title: 'Eat Spinner',
+              icon: const Icon(Icons.gamepad_outlined, color: Color(0xFF626262)),
+              onTap: (){},
+            ),
+            ProfileMenuItem(
+              title: 'Share Profile Link',
+              icon: const Icon(Icons.link_outlined, color: Color(0xFF626262)),
+              onTap: (){},
+            ),
+            ProfileMenuItem(
+              title: 'Logout',
+              icon: const Icon(Icons.logout_outlined, color: Color(0xFF626262)),
+              onTap: (){},
+            )
+          ],
         ),
       ),
     );
